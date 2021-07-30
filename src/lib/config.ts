b78e1466ce114adff9609cdef6bd122225f36298
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-30 19:25:28 +0530

    Add Vercel adapter

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,11 @@
+import { mode } from '$app/env'
+
 export const appConfig = {
     notion: {
-        token: String(import.meta.env.VITE_NOTION_TOKEN),
-        databaseId: String(import.meta.env.VITE_NOTION_DATABASE_ID)
+        token: mode === 'development' ? String(import.meta.env.VITE_NOTION_TOKEN) : process.env.NOTION_TOKEN,
+        databaseId: mode === 'development' ? String(import.meta.env.VITE_NOTION_DATABASE_ID) : process.env.NOTION_DATABASE_ID
     },
-    baseUrl: import.meta.env.VITE_APP_URL ? String(import.meta.env.VITE_APP_URL) : 'http://localhost:3001' ,
+    baseUrl: mode === 'development' ? (import.meta.env.VITE_APP_URL ? String(import.meta.env.VITE_APP_URL) : '') : process.env.APP_URL ?  process.env.APP_URL : '',
     properties: {
         title: 'title',
         summary: 'summary',
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import sveltePreprocess from 'svelte-preprocess';
-import node from '@sveltejs/adapter-node';
+import sveltePreprocess from 'svelte-preprocess'
+import vercel from '@sveltejs/adapter-vercel';
+// import node from '@sveltejs/adapter-node'
+// import adapterStatic from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -17,14 +19,14 @@ export default {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: node(),
+        adapter: vercel(),
+		// adapter: adapterStatic({
+        //     pages: 'build',
+        //     assets: 'build',
+        //     fallback: null
+        // }),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-
-		vite: {
-			ssr: {
-			}
-		}
 	}
 };
